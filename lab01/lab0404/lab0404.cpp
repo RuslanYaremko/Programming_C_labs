4fe0a76edc04a9e73dd3b08ea30ff3cff98f459d
Ruslan Yaremko <yaremkoruslan69@gmail.com>
2022-02-20 23:09:58 +0200

    lab0404

---
+++ lab01/lab0404/lab0404.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int V1, V2, S, T, V;
+
+	cout << "V1" << endl;
+	cin >> V1;
+	cout << "V2" << endl;
+	cin >> V2;
+	cout << "S" << endl;
+	cin >> S;
+	cout << "T" << endl;
+	cin >> T;
+	cout << "V" << endl;
+	cin >> V;
+	cout << V1 + V2;
+
+	return 0;
+}
