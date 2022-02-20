3d0480212fed6192528d50573355d837cc35a380
Ruslan Yaremko <yaremkoruslan69@gmail.com>
2022-02-20 21:28:26 +0200

    lab0304

---
+++ lab01/lab0304/lab0304.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+int n;
+
+cout << "side";
+cin >> n;
+cout << endl;
+cout << "P = " << 3 * n << endl;
+cout << "h = " << (n * sqrt(3)) / 2 << endl;
+
+cout << "R = " << (n * sqrt(3)) / 3 << endl;
+cout << "r = " << (n * sqrt(6)) / 6 << endl;
+return 0;
+}
