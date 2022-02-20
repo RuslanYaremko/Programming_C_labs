d82bdad80489ab6d6d2d084f1e3ea15933e48f10
Ruslan Yaremko <yaremkoruslan69@gmail.com>
2022-02-20 22:12:22 +0200

    lab0508

---
+++ lab01/lab0508/lab0508.cpp
@@ -0,0 +1,17 @@
+﻿#include<cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	float a, b, alf, z1, z2;
+	cout << "Ввести альфа" << endl;
+	cin >> alf;
+	a = sin(3.14 / 2 + 3 * alf);
+	b = 1 - sin(3 * alf - 3.14);
+	z1 = a / b;
+	z2 = tan((5 / 4 * 3.14) + (3 / 2 * alf));
+	cout << "z1=" << z1 << endl;
+	cout << "z2=" << z2;
+}
