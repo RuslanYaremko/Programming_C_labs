7c1e4cd0e2b28affa883c63ed060f136e0d62c44
Ruslan Yaremko <yaremkoruslan69@gmail.com>
2022-04-15 11:36:36 +0300

    0207

--- lab02/lab0207/lab0207.cpp
+++ lab02/lab0207/lab0207.cpp
@@ -1,9 +1,27 @@
 ﻿#include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
     int x;
+    cout << "Enter x" << endl;
+    cin >> x;
+    cout << tan(x) << endl;
+    cout << 1 + abs(x) << endl;
+    cout << (1 + x*2)*4 << endl;
 
+    if (tan(x) < 1 + abs(x) && tan(x) < (1 + x * 2) * 4)
+    {
+        cout << tan(x) << endl;
+    }
+    if (1 + abs(x)< tan(x) && 1 + abs(x)< (1 + x * 2) * 4)
+    {
+        cout << (1 + abs(x)) << endl;
+    }
+    if ((1 + x * 2) *4 < tan(x) && (1 + x * 2) < 1 + abs(x))
+    {
+        cout << (1 + abs(x)) << endl;
+    }
+  
 }
