83df6d8a4afb14a22a501175cd1ca62059ef537b
RuslanYaremko <99643936+RuslanYaremko@users.noreply.github.com>
2022-04-20 11:00:12 +0300

    Add files via upload

---
+++ lab02/lab020114/lab020114.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int F, a, b, c, x;
+
+	cout << "Enter F" << endl;
+	cin >> F;
+	cout << "Enter a" << endl;
+	cin >> a;
+	cout << "Enter b" << endl;
+	cin >> b;
+	cout << "Enter c" << endl;
+	cin >> c;
+	cout << "Enter x" << endl;
+	cin >> x;
+
+	if ((x + c) < 0 && (a != 0)) {
+		F = (((-a * x) * 3) - b);
+	}
+	else if ((x + c) > 0 && (a != 0)) {
+		F = ((x - a) / (x - c));
+	}
+	else {
+		F = (x / c) + (c / x);
+	}
+	cout << F;
+
+}
