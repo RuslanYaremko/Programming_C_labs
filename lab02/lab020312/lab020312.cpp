f1526ba40c545ce1e162950fb340a40108658196
RuslanYaremko <99643936+RuslanYaremko@users.noreply.github.com>
2022-05-01 18:53:08 +0300

    Add files via upload

---
+++ lab02/lab020312/lab020312.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+	int n, i, s = 1;
+	cout << "Enter n"<< endl;
+	cin >> n;
+
+	for (i = 1; i <= 1; i += 1)
+		s = s * (1 + 2 / cos(1 + 2));
+	cout << "Result =" << s;
+
+ }
